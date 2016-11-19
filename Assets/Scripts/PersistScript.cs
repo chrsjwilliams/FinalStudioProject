77625a77bfc1912918f58c1eaa03e6df3ce70ec3
chjwilliams <c.j.williams717@gmail.com>
2016-11-19 00:51:19 -0500

    Inital File Commit
    Structure was given to Taylor’s previous game. This was done to make it
    easier to navigate the project. Scaffolding for the restaurant game was
    added. No scripts were written yet.
    
    Scenes Added:
    SplashScreen
    Restaurant_Drinks
    Restaurant_Appetizer
    Restaurant_Entree
    Restaurant_Desert
    EndState_Failure
    EndState_Success
    
    Scenes Removed:
    None
    
    Scripts Added:
    None
    
    Scripts Removed:
    None
    
    Assets Added:
    None
    
    Assets Removed:
    238529__waveplay__improvised-chill-8.wav
    (reason: file was too large to upload to GitHub)
    
    Known Bugs/Issues:
    None

---
+++ Assets/Scripts/PersistScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistScript : MonoBehaviour {
+
+	private static PersistScript persistScript;
+
+	void Awake () {
+		if (persistScript == null) {
+			DontDestroyOnLoad (this);
+			persistScript = this;
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class RestartScript : MonoBehaviour
+{
+	public void StartGame()
+	{
+		// "Stage1" is the name of the first scene we created.
+		Application.LoadLevel("First");
+	}
+}
