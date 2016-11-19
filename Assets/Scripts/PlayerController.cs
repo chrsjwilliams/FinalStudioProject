8dedc44c802190abe091b5d7638a69fbd82517b1
chjwilliams <c.j.williams717@gmail.com>
2016-11-19 13:54:13 -0500

    Player-side Food Action Implemented
    Added player’s basic interaction with food. When player eats food “Self
    Image” goes down. The bars are for debug purposes only and will not
    make an appearance in the released version of the game. Turned game
    objects into UI objects for scaling purposes.
    
    Scenes Added:
    None
    
    Scenes Removed:
    None
    
    Scripts Added:
    Player Controller Script
    (Contains empty functions of what the player can do)
    
    Scripts Removed:
    None
    
    Assets Added:
    Plate_Placeholder.png
    Glass_Placeholder.png
    
    Assets Removed:
    None
    
    GameObjects/UI Objects Added:
    Player [GO]
    Canvas [UI]
    Background [UI]
    Water [UI]
    Food [UI]
    SelfImage Slider [UI]
    Composure Slider [UI]
    EventSystem [UI]
    
    GameObjects/UI Objects Removed:
    None

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	PlayerController: 																	*/
+/*																						*/
+/*			Functions:																	*/
+/*					Start()																*/
+/*					LoseComposure()														*/
+/*					GainComposure()														*/
+/*					LowerSelfImage()													*/
+/*					GoToRestroom()														*/
+/*					PlayWithFood()														*/
+/*					EatFood()															*/
+/*					Update()															*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+
+public class PlayerController : MonoBehaviour 
+{
+
+	//	Public Variables
+	public float playerComposure;
+	public float playerSelfImage;
+	public Slider playerComposureBar;
+	public Slider playerSelfImageBar;
+
+	//	Private Variables
+	[SerializeField] private float m_StartingComposure;
+	[SerializeField] private float m_StartingSelfImage;
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	Start: Runs once at the begining of the game. Initalizes variables.					*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void Start () 
+	{
+		m_StartingComposure = 10f;
+		m_StartingSelfImage = 10f;
+		playerComposure = m_StartingComposure;
+		playerSelfImage = m_StartingSelfImage;
+
+		//	These sliders are for debugging purposes only. They will not be in the release version of the game.
+		playerComposureBar = GameObject.Find ("Composure Slider").GetComponent<Slider> ();
+		playerSelfImageBar = GameObject.Find ("Self Image Slider").GetComponent<Slider> ();
+	
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	LoseComposure: 																		*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void LoseComposure()
+	{
+		playerComposure--;
+		playerComposureBar.value = playerComposure;
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	GainComposure: 																		*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void GainComposure()
+	{
+		playerComposure++;
+		playerComposureBar.value = playerComposure;
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	LowerSelfImage: 																	*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void LowerSelfImage()
+	{
+		playerSelfImage--;
+		playerSelfImageBar.value = playerSelfImage;
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	GoToRestroom: 																		*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void GoToRestroom()
+	{
+		
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	PlayWithFood: 																		*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void PlayWithFood()
+	{
+
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	EatFood: 																			*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/	
+	void EatFood()
+	{
+		LowerSelfImage ();
+	}
+
+/*--------------------------------------------------------------------------------------*/
+/*																						*/
+/*	Update: Called once per frame														*/
+/*																						*/
+/*--------------------------------------------------------------------------------------*/
+	void Update () 
+	{
+	
+	}
+}
